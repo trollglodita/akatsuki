42e71e623aeebbdd9706e1bb86d34e614c7d72e5
Trollglodita <1trollglodita@gmail.com>
2014-07-18 19:25:44 -0400

    fixing popup load page outside device ready

--- js/app.js
+++ js/app.js
@@ -1,56 +1,5 @@
 document.addEventListener("deviceready", function () {
-	function scale( width, height, padding, border ) {
-	    var scrWidth = $( window ).width() - 30,
-	        scrHeight = $( window ).height() - 30,
-	        ifrPadding = 2 * padding,
-	        ifrBorder = 2 * border,
-	        ifrWidth = width + ifrPadding + ifrBorder,
-	        ifrHeight = height + ifrPadding + ifrBorder,
-	        h, w;
-	 
-	    if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
-	        h = ifrHeight;
-	        w = ifrWidth;
-	    } else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
-	        h = ( scrWidth / ifrWidth ) * ifrHeight;
-	        w = scrWidth;
-	    } else {
-	        h = scrHeight;
-	        w = ( scrHeight / ifrHeight ) * ifrWidth;
-	    }
-	 
-	    return {
-	        'width': w - ( ifrPadding + ifrBorder ),
-	        'height': h - ( ifrPadding + ifrBorder )
-	    };
-	};
 
-	$( document ).on( "pageinit", function() {
-	    $( "#popupMap iframe" )
-	        .attr( "width", 0 )
-	        .attr( "height", 0 );
-	 
-	    $( "#popupMap" ).on({
-	        popupbeforeposition: function() {
-	            var size = scale( 497, 298, 15, 1 ),
-	                w = size.width,
-	                h = size.height;
-	 
-	            $( "#popupMap iframe" )
-	                .attr( "width", w )
-	                .attr( "height", window.innerHeight - 55 );
-	        },
-	        popupafterclose: function() {
-	            $( "#popupMap iframe" )
-	                .attr( "width", 0 )
-	                .attr( "height", 0 );
-	        }
-	    });
-	    setTimeout(function () {
-			$("#popupMap").popup("open");
-			console.log("Popup called");
-		}, 5000);
-	});
 	jQuery( "#prev" ).on( "vmousedown", function( e ) { e.preventDefault(); } );
 
 	document.addEventListener('touchmove', function (e) {
@@ -139,6 +88,58 @@ document.addEventListener("deviceready", function () {
 	
 },false);
 
+function scale( width, height, padding, border ) {
+	    var scrWidth = $( window ).width() - 30,
+	        scrHeight = $( window ).height() - 30,
+	        ifrPadding = 2 * padding,
+	        ifrBorder = 2 * border,
+	        ifrWidth = width + ifrPadding + ifrBorder,
+	        ifrHeight = height + ifrPadding + ifrBorder,
+	        h, w;
+	 
+	    if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
+	        h = ifrHeight;
+	        w = ifrWidth;
+	    } else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
+	        h = ( scrWidth / ifrWidth ) * ifrHeight;
+	        w = scrWidth;
+	    } else {
+	        h = scrHeight;
+	        w = ( scrHeight / ifrHeight ) * ifrWidth;
+	    }
+	 
+	    return {
+	        'width': w - ( ifrPadding + ifrBorder ),
+	        'height': h - ( ifrPadding + ifrBorder )
+	    };
+	};
+
+	$( document ).on( "pageinit", function() {
+	    $( "#popupMap iframe" )
+	        .attr( "width", 0 )
+	        .attr( "height", 0 );
+	 
+	    $( "#popupMap" ).on({
+	        popupbeforeposition: function() {
+	            var size = scale( 497, 298, 15, 1 ),
+	                w = size.width,
+	                h = size.height;
+	 
+	            $( "#popupMap iframe" )
+	                .attr( "width", w )
+	                .attr( "height", window.innerHeight - 55 );
+	        },
+	        popupafterclose: function() {
+	            $( "#popupMap iframe" )
+	                .attr( "width", 0 )
+	                .attr( "height", 0 );
+	        }
+	    });
+	    setTimeout(function () {
+			$("#popupMap").popup("open");
+			console.log("Popup called");
+		}, 5000);
+	});
 
 
 
