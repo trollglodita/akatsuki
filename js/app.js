0c30d3842b1477708705c04ab72e8bbfd01df437
Trollglodita <1trollglodita@gmail.com>
2014-09-16 02:45:27 -0300

    Ads and default alerts

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ document.addEventListener("deviceready", function () {
 	});
 
 	$("#change").change(function() {
-	        alert('Load samples feature will be included ASAP!');
-	    }); 
+        alert('Load samples feature will be included ASAP!');
+    }); 
 
 
 
@@ -80,66 +80,73 @@ document.addEventListener("deviceready", function () {
 
 	var toolbar = new Toolbar('launchpad');
 	var loads = document.querySelectorAll('.nox');
-	for (var i = 0; i < loads.length; i++) {
+	for (var i = 0, len = loads.length; i < len; i++) {
 		loads[i].style.width = window.innerWidth/3.3 + 'px';
 		loads[i].style.height = window.innerWidth/3.3 + 'px';
 	};
-	
+
+	var soon = document.querySelectorAll('.soon');
+	for (var i = 0, len = soon.length; i < len; i++) {
+		soon[i].addEventListener('click', function () {
+			alert('This will be ready with the Loading Samples Feature');
+		});
+	}
+
 	
 },false);
 
 function scale( width, height, padding, border ) {
-	    var scrWidth = $( window ).width() - 30,
-	        scrHeight = $( window ).height() - 30,
-	        ifrPadding = 2 * padding,
-	        ifrBorder = 2 * border,
-	        ifrWidth = width + ifrPadding + ifrBorder,
-	        ifrHeight = height + ifrPadding + ifrBorder,
-	        h, w;
-	 
-	    if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
-	        h = ifrHeight;
-	        w = ifrWidth;
-	    } else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
-	        h = ( scrWidth / ifrWidth ) * ifrHeight;
-	        w = scrWidth;
-	    } else {
-	        h = scrHeight;
-	        w = ( scrHeight / ifrHeight ) * ifrWidth;
-	    }
-	 
-	    return {
-	        'width': w - ( ifrPadding + ifrBorder ),
-	        'height': h - ( ifrPadding + ifrBorder )
-	    };
-	};
+    var scrWidth = $( window ).width() - 30,
+        scrHeight = $( window ).height() - 30,
+        ifrPadding = 2 * padding,
+        ifrBorder = 2 * border,
+        ifrWidth = width + ifrPadding + ifrBorder,
+        ifrHeight = height + ifrPadding + ifrBorder,
+        h, w;
+ 
+    if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
+        h = ifrHeight;
+        w = ifrWidth;
+    } else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
+        h = ( scrWidth / ifrWidth ) * ifrHeight;
+        w = scrWidth;
+    } else {
+        h = scrHeight;
+        w = ( scrHeight / ifrHeight ) * ifrWidth;
+    }
+ 
+    return {
+        'width': w - ( ifrPadding + ifrBorder ),
+        'height': h - ( ifrPadding + ifrBorder )
+    };
+};
 
-	$( document ).on( "pageinit", function() {
-	    $( "#popupMap iframe" )
-	        .attr( "width", 0 )
-	        .attr( "height", 0 );
-	 
-	    $( "#popupMap" ).on({
-	        popupbeforeposition: function() {
-	            var size = scale( 497, 298, 15, 1 ),
-	                w = size.width,
-	                h = size.height;
-	 
-	            $( "#popupMap iframe" )
-	                .attr( "width", w )
-	                .attr( "height", window.innerHeight - 55 );
-	        },
-	        popupafterclose: function() {
-	            $( "#popupMap iframe" )
-	                .attr( "width", 0 )
-	                .attr( "height", 0 );
-	        }
-	    });
-	    setTimeout(function () {
-			$("#popupMap").popup("open");
-			console.log("Popup called");
-		}, 5000);
-	});
+$( document ).on( "pageinit", function() {
+    $( "#popupMap iframe" )
+        .attr( "width", 0 )
+        .attr( "height", 0 );
+ 
+    $( "#popupMap" ).on({
+        popupbeforeposition: function() {
+            var size = scale( 497, 298, 15, 1 ),
+                w = size.width,
+                h = size.height;
+ 
+            $( "#popupMap iframe" )
+                .attr( "width", w )
+                .attr( "height", window.innerHeight - 55 );
+        },
+        popupafterclose: function() {
+            $( "#popupMap iframe" )
+                .attr( "width", 0 )
+                .attr( "height", 0 );
+        }
+    });
+    setTimeout(function () {
+		$("#popupMap").popup("open");
+		console.log("Popup called");
+	}, 5000);
+});
 
 
 
